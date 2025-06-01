e5798630d5b58d5f6b8e3f65f873c1f085c15329
kishorerayuduamain <kishorerayudumain@gmail.com>
2025-06-01 18:07:14 -0500

    updated

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -30,7 +30,7 @@ const Navigation = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div className="text-2xl font-bold gradient-text">
-            QA Automation Wizard
+            QA Automation Tester
           </div>
 
           {/* Desktop Navigation */}
